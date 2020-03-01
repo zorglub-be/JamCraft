951efdb4c888171c7ad34ce2cb6b1ba027daf997
Gerald <infernumex@gmail.com>
2020-03-01 08:05:46 -0800

    Switch to URP
    Upgraded project to use URP. Any sprites which use the Sprites-Default shader will have to be modified to use the Sprites-Lit-Default in the root directory.
    Also added another test scene with lighting effects.

---
+++ Assets/Scripts/LightFlicker2D.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using UnityEngine.Experimental.Rendering.Universal;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class LightFlicker2D : MonoBehaviour
+{
+    [SerializeField] private float timeScale = 4f;
+    
+    private float _intensity = 1.5f;
+    private Light2D _light2D;
+    private float _randomSeed;
+    private float _timer = 0f;
+
+    void Awake()
+    {
+        _light2D = GetComponent<Light2D>();
+        _randomSeed = Random.Range(1, 9999);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _timer += Time.deltaTime;
+        
+        if (_timer > Random.Range(5, 10))
+        {
+            StartCoroutine(Flicker());
+        }
+
+        if (_timer > 10 ) _timer = 0;
+        }
+
+    private IEnumerator Flicker()
+    {
+        float intensity = _intensity *
+                          Mathf.PerlinNoise(Random.Range(Time.time * timeScale, Time.time * timeScale), _randomSeed);
+        _light2D.intensity = intensity;
+        
+        yield return null;
+    }
+}
