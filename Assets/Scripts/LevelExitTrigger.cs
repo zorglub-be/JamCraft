ba0e609cbde64faa2f1444a1af358e63c030d14d
Gerald <infernumex@gmail.com>
2020-03-21 13:33:54 -0700

    See Description
    -Added level transition triggers and script
    -Main Menu/New Game button now loads Level 1
    -Added destructible banner script and audio
    -Added all UI and Level scenes to build settings
    -Fixed exit door trigger collider (wasn't a trigger)

---
+++ Assets/Scripts/LevelExitTrigger.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelExitTrigger : MonoBehaviour
+{
+    [Tooltip("Look at the build settings for build index.")]
+    [SerializeField] private int nextLevelToLoad;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(nextLevelToLoad);
+        }
+    }
+}
--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -11,6 +11,7 @@ public class MainMenu : MonoBehaviour
     public void StartNewGame()
     {
         Debug.Log("Started New Game");
+        SceneManager.LoadScene("Level1");
     }
 
     public void ContinueFromLastSave()
