80e0a38cd42462c1eb4e6d6d22baca5e4d32d18f
WaltDjr <waltdjr@gmail.com>
2020-03-29 21:32:21 -0400

    Add Female Character Asset

---
+++ Assets/External/2D Customizable Characters/Characters/Female/Editor/FemaleCharacterSetupEditor.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(FemaleCharacterSetup))]
+public class FemaleCharacterSetupEditor : Editor {
+
+    public override void OnInspectorGUI()
+    {
+        if (!Application.isPlaying)
+        {
+			FemaleCharacterSetup characterSetup = target as FemaleCharacterSetup;
+            if (GUILayout.Button("Update Sprites"))
+            {
+                characterSetup.UpdateSprites(false);
+            }
+        }
+
+        base.OnInspectorGUI();
+    }
+}
